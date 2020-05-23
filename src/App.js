15e6591e908cdecaf5fd3ad7df80e55029943798
Nnamani Ugochukwu F <royal.ugoh@gmail.com>
2020-05-23 09:47:56 +0100

    modified App component to make an api call and update state and add styles to app.css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import { robots } from './robots';
+//import { robots } from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import './App.css';
 
 class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
       searchInput: '',
-      robots: robots
+      robots: []
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => this.setState({robots: data}))
+  }
+
   handleSearch = (event) => {
     this.setState({
       searchInput: event.target.value
@@ -27,7 +36,9 @@ class App extends Component{
         <h1>RoboFriends</h1>
         <SearchBox inputField={this.state.searchInput}
         handleChange={this.handleSearch} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     )
   }
